7d15fb3399a8ee867ce57bdb2441f32fd7610abf
MilenaMilena1 <1132226152@DESKTOP-U8GVLCM>
2024-02-29 13:24:11 +0300

    .

--- main.cpp
+++ main.cpp
@@ -1,1 +1,41 @@
-test
+#include <iostream>
+using namespace std;
+
+class Shape {
+public:
+    Scale(float scaleFactor) :void virtual = 0;
+    ShowInfo() :void virtual = 0;
+    void GetName() :string virtual = 0;
+}
+    private:
+
+};
+
+class Shape2D : public Shape {
+public:
+
+private:
+};
+
+class Shape3D : public Shape {
+
+};
+
+class Triangle : public Shape {
+
+private:
+    double base;
+    double height;
+
+public:
+    Triangle(double b, double h) : base(b), height(h) {}
+
+    double calculateArea() const override {
+        double s = (side1 + side2 + side3) / 2;
+        return sqrt(s * (s - side1) * (s - side2) * (s - side3));
+    }
+
+    double calculatePerimeter() const override {
+        return side1 + side2 + side3;
+    }
+};
